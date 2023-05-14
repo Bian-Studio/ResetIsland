35976413e9a42948ca646a8c029f7df6f9e85574
WinExp <1450422389@qq.com>
2023-05-14 17:45:14 +0800

    + 模式 1

--- ResetIsland/MainWindow.xaml.cs
+++ ResetIsland/MainWindow.xaml.cs
@@ -24,5 +24,8 @@ namespace ResetIsland
         {
             InitializeComponent();
         }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+            => this.DragMove();
     }
 }
---
+++ ResetIsland/Pages/Page1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResetIsland.Pages
+{
+    /// <summary>
+    /// Page1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+            this.Loaded += delegate
+            {
+                // 时间同步任务
+                Task.Run(async () =>
+                {
+                    Dispatcher.Invoke(() => // 使用 UI 线程同步时间
+                    {
+                        TimeText.Text = DateTime.Now.ToString("HH:mm");
+                    });
+                    await Task.Delay(10);
+                });
+            };
+        }
+    }
+}
